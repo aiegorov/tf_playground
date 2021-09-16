b2c4ca557a0dbd14d7398ca02818aab4553d188b
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2021-09-16 23:12:42 +0200

    Add dummy code for a neural network

---
+++ net_def/net.py
@@ -0,0 +1,2 @@
+def conv_layer():
+    pass
---
+++ net_def/test_net.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class NetTest(unittest.TestCase):
+    def test_conv_layer(self):
+        self.fail()
+
+
+if __name__ == '__main__':
+    unittest.main()
