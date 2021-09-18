c6f13b652bdd265ea657ca658943744a52247528
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2021-09-18 22:55:28 +0200

    WIP writing test for the conv layers etc.

--- net_def/test_net.py
+++ net_def/test_net.py
@@ -1,9 +1,17 @@
 import unittest
 
+class KerasTest(unittest.TestCase):
+    """Let's just test some keras stuff, just for fun, why not"""
+
+    def test_conv2d(self):
+        """Testing the good old conv2d layer"""
+        layer = None
+        self.fail()
+
 
 class NetTest(unittest.TestCase):
     def test_conv_layer(self):
-        self.fail()
+        pass
 
 
 if __name__ == '__main__':
