3e82a52db75a4046834ae0f67af987faf714f28f
Andrii Iegorov <andrii.iegorov.mm@gmail.com>
2021-09-19 17:43:32 +0200

    Add test for the TF Conv layer - just for fun

--- net_def/test_net.py
+++ net_def/test_net.py
@@ -1,12 +1,21 @@
 import unittest
 
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D
+
+
 class KerasTest(unittest.TestCase):
     """Let's just test some keras stuff, just for fun, why not"""
 
     def test_conv2d(self):
         """Testing the good old conv2d layer"""
-        layer = None
-        self.fail()
+        input = tf.random.normal((1, 10, 10, 4))
+        conv_layer = Conv2D(filters=5, kernel_size=(3,3), padding="same")
+        output = conv_layer(input)
+        self.assertEqual(output.shape, (1, 10, 10, 5))
+        weights, biases = conv_layer.weights
+        self.assertEqual(weights.shape, (3, 3, 4, 5))
+        self.assertEqual(biases.shape, (5,))
 
 
 class NetTest(unittest.TestCase):
